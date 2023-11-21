bc6df2a41896b5bcfac6feab7472f4dd52d18869
tpazz <tpazzz98@gmail.com>
2023-11-21 18:51:32 +0100

    Parallel Runner classes

--- src/test/java/org/example/core/base/AcceptanceTestSuite.java
+++
@@ -1,9 +0,0 @@
-package org.example.core.base;
-
-import cucumber.api.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/", glue="org.example", tags = "@action_steps_example")
-public class AcceptanceTestSuite {}
