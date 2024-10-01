665fd07c734c0bdfb468ac0b57e3861ed5e4a5f7
tpazz <tpazzz98@gmail.com>
2024-10-01 18:35:34 +0200

    Constants project resources edit

--- src/test/java/org/example/core/base/Constants.java
+++ src/test/java/org/example/core/base/Constants.java
@@ -3,12 +3,12 @@ package org.example.core.base;
 public class Constants {
 
     // DIRECTORIES
-    public static final String USER_HOME = System.getProperty("user.home");
-    public static final String USER_ENV_HOME = System.getenv("SYSTEM_DEFAULTWORKINGDIRECTORY");
+    public static final String USER_HOME = System.getProperty("user.home"); // checking local installation of browsers (non-pipeline)
+    public static final String USER_ENV_HOME = System.getenv("SYSTEM_DEFAULTWORKINGDIRECTORY"); // checking local installation of browsers (pipeline)
     public static final String USER_DIR = System.getProperty("user.dir");
     public static final String LOCAL_DOWNLOADS = USER_HOME + "\\Downloads\\";
     public static final String PROJECT_RESOURCES_WINDOWS = USER_DIR + "\\src\\test\\resources\\webdriver\\windows\\";
-    public static final String PROJECT_RESOURCES_LINUX = USER_ENV_HOME + "/src/test/resources/webdriver/linux/";
+    public static final String PROJECT_RESOURCES_LINUX = USER_DIR + "/src/test/resources/webdriver/linux/";
 
     // OTHER URLS
     public static final String CHROME_DRIVER_API = "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json";
